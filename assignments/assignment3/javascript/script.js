70ffdcff009ef858884e6338a46e1b8397549dd3
Andy Kukuc <jeep98.ak@gmail.com>
2019-09-06 11:28:29 -0500

    getting javascript and html to work

--- assignments/assignment3/javascript/script.js
+++ assignments/assignment3/javascript/script.js
@@ -2,4 +2,8 @@
  * Professor Charles Beck
  * ITMD-462-01
  * 6th Sept. 2019*/
-
+function display_test(){
+  var course1=["ITMD","462","Web Application Development"];
+  var webapp=course1.toString();
+  document.getElementById("test").innerHTML=webapp;
+}
