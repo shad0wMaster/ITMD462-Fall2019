1793b1bba991aa2423c47d512bbf4400b1747a5c
Andy Kukuc <jeep98.ak@gmail.com>
2019-10-18 12:10:02 -0500

    was able to get the database to hash the passwords

--- assignments/final_project/info.php
+++ assignments/final_project/info.php
@@ -3,9 +3,8 @@ $servername="localhost:3306";
 $username=
 $password=
 $dbname=;
-
 //Creating the connection FUCK YOU PHP
-$connection=new mysqli($servername,$username,$password,$dbname);
+$connection=new mysqli($dbservername,$dbusername,$dbpassword,$dbname);
 
 //Check connection please for the love of god work
 if($connection->connect_error){
@@ -15,12 +14,14 @@ if($connection->connect_error){
 }
 
 //sql connection oh yeh
-$sql="INSERT INTO users (username,email) VALUES ('".$_POST["username1"]."','".$_POST["email"]."')";
-if(mysqli_query($connection,$sql)){
-  //echo("Username for HTML INPUT: ".$_POST['username1']."<br/>\n"); Used for testing here
+$sql="INSERT INTO new_users(firstname,lastname,email,password) VALUES ('".$_POST["firstname"]."','".$_POST["lastname"]."','".$_POST["email"]."','".$_POST["password"]."')";
+$sql_hash="update new_users set password=SHA(password)";
+if(mysqli_query($connection,$sql)&& mysqli_query($connection,$sql_hash)){
+  echo("Username for HTML INPUT: ".$_POST['firstname']."<br/>\n"); //Used for testing here
   echo "\n\nRecords Added to the Database!";
 }else{
   echo "ERROR: SQL Syntax Failed $sql.".mysqli_query($connection);
+  //echo "Hashing Failed: $sql_hash".mysqli_query($connection);
 }
 mysqli_close($connection);
 
