fef95e4eb6e92a66ab9cd080ef16d6b94c9d3740
Andy Kukuc <jeep98.ak@gmail.com>
2019-11-01 10:35:58 -0500

    finishing up and will be doing some testing

--- assignments/assignment5/routes/users.js
+++ assignments/assignment5/routes/users.js
@@ -6,6 +6,8 @@
 //users.js file here we go
 const express=require('express');
 const router=express.Router();
+//here we are using get with users separate everything to not get confused.
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 router.get('/',(req,res,next)=>{
   res.status(200).json({
     message:'Handling GET requests to /users'
@@ -16,6 +18,8 @@ router.post('/',(req,res,next)=>{
     message:'Handling POST REQUESTS to /users'
   });
 });
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//users id is set here here its all written for the get request
 router.get('/:usersID',(req,res,next)=>{
   const id=req.params.usersID;
   if(id==='andy'){
@@ -23,8 +27,36 @@ router.get('/:usersID',(req,res,next)=>{
       message:'You discovered userID Andy using Get!',id:id
     });
   }else{
-    res.status(200).json{
+    res.status(200).json({
       message:'Normal ID Has Been Passed Using Get!',id:id
+    });
+  };
+});
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//users id here is set and here ware using the post request
+router.post('/:usersID',(req,res,next)=>{
+  const id=req.params.usersID;
+  if(id==='chuck'){
+    res.status(200).json({
+      message:'Specific usersID passed using POST!',id:id
+    });
+  }else{
+    res.status(202).json({
+      message:'Normal usersID passed using POST!',id:id
+    });
+  };
+});
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*usersID set to patch*/
+router.patch('/:usersID',(req,res,next)=>{
+  const id=req.params.usersID;
+  if(id==='patch'){
+    res.status(200).json({
+      message:'Specific usersID passed using PATCH!',id:id
+    });
+  }else{
+    res.status(203).json({
+      message:'Normal usersID passed using PATCH!',id:id
     });
   };
 });
