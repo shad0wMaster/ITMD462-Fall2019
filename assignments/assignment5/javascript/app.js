1c74106b42656ad4871c14a2b5814f8a57a42632
Andy Kukuc <jeep98.ak@gmail.com>
2019-10-31 20:51:37 -0500

    testing everything and fianlly figured out how verything works

--- assignments/assignment5/javascript/app.js
+++ assignments/assignment5/javascript/app.js
@@ -7,11 +7,13 @@ const express=require('express');
 const app=express();
 const morgan=require('morgan');
 const productRoutes=require('../routes/products.js');
-const orderRoutes = require('../routes/orders.js');
+const orderRoutes=require('../routes/orders.js');
+const userRoutes=require('../routes/users.js')
 app.use(morgan('dev'));
 // Routes which should handle requests
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
+app.use('/users',userRoutes);
 app.use((req,res,next)=>{
     const error=new Error('Not Found');
     error.status=404;
