ff499cd21eae148da1c8d520ec9af18d95330adc
Andy Kukuc <jeep98.ak@gmail.com>
2019-11-01 09:53:14 -0500

    testing usersID now

--- assignments/assignment5/routes/users.js
+++ assignments/assignment5/routes/users.js
@@ -16,4 +16,16 @@ router.post('/',(req,res,next)=>{
     message:'Handling POST REQUESTS to /users'
   });
 });
+router.get('/:usersID',(req,res,next)=>{
+  const id=req.params.usersID;
+  if(id==='andy'){
+    res.status(200).json({
+      message:'You discovered userID Andy using Get!',id:id
+    });
+  }else{
+    res.status(200).json{
+      message:'Normal ID Has Been Passed Using Get!',id:id
+    });
+  };
+});
 module.exports=router;
