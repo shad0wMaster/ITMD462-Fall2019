727c4e807baf21ea0beeb609053a5fb8ec2f173a
Andy Kukuc <jeep98.ak@gmail.com>
2019-11-04 16:26:54 -0600

    changed all if else statements to while loops don't lnow why

--- assignments/assignment5/routes/users.js
+++ assignments/assignment5/routes/users.js
@@ -6,74 +6,55 @@
 //users.js file here we go
 const express=require('express');
 const router=express.Router();
+const array1=['andy','iit','dog','chuck'];
 //here we are using get with users separate everything to not get confused.
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 router.get('/',(req,res,next)=>{
   res.status(200).json({
-    message:'Handling GET requests to /users'
+    message:'Handling GET requests to /users!'
   });
 });
 router.post('/',(req,res,next)=>{
   res.status(201).json({
-    message:'Handling POST REQUESTS to /users'
+    message:'Handling POST requests to /users!'
   });
 });
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //users id is set here here its all written for the get request
 router.get('/:usersID',(req,res,next)=>{
-  var array1=['andy','iit','dog'];
   const id=req.params.usersID;
-   if(array1.indexOf(id)>-1){
-    res.status(200).json({
-      message:'You discovered a secret userID using Get!'
-    });
-  }else{
-    res.status(201).json({
-      message:'Normal ID Has Been Passed Using Get!'
-    });
-  };
+  while(array1.indexOf(id)>-1){
+    res.status(200).json({message:'You discovered a secret userID using Get!',id:id});
+  }
+  res.status(201).json({message:'Normal ID Has Been Passed Using Get!',id:id});
 });
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //users id here is set and here ware using the post request
 router.post('/:usersID',(req,res,next)=>{
   const id=req.params.usersID;
-  if(id==='chuck'){
-    res.status(200).json({
-      message:'Specific usersID passed using POST!',id:id
-    });
-  }else{
-    res.status(202).json({
-      message:'Normal usersID passed using POST!',id:id
-    });
-  };
+  while(array1.indexOf(id)>-1){
+    res.status(200).json({message:'Specific usersID passed using POST!',id:id});
+  }
+    res.status(202).json({message:'Normal usersID passed using POST!',id:id});
 });
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*usersID set to patch*/
 router.patch('/:usersID',(req,res,next)=>{
   const id=req.params.usersID;
-  if(id==='patch'){
-    res.status(200).json({
-      message:'Specific usersID passed using PATCH!',id:id
-    });
-  }else{
-    res.status(203).json({
-      message:'Normal usersID passed using PATCH!',id:id
-    });
-  };
+  while(array1.indexOf(id)>-1){
+    res.status(200).json({message:'Specific usersID passed using PATCH!',id:id});
+  }
+    res.status(203).json({message:'Normal usersID passed using PATCH!',id:id});
 });
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*usersID set to delete*/
 router.delete('/:usersID',(req,res,next)=>{
   const id=req.params.usersID;
-  if(id==='delete'){
-    res.status(200).json({
-      message:'Specific usersID passed using DELETE!',id:id
-    });
-  }else{
-    res.status(203).json({
-      message:'Normal usersID passed using DELETE!',id:id
-    });
-  };
+  while(array1.indexOf(id)>-1){
+    res.status(200).json({message:'Specific usersID passed using DELETE!',id:id});
+  }
+  res.status(203).json({
+    message:'Normal usersID passed using DELETE!',id:id});
 });
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 module.exports=router;
