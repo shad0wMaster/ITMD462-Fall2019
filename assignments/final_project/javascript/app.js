36320b700e60366bfebea8952578675fadef99a6
Andy Kukuc <jeep98.ak@gmail.com>
2019-11-14 19:13:59 -0600

    oh god how am I going to do this project

--- assignments/final_project/javascript/app.js
+++ assignments/final_project/javascript/app.js
@@ -4,11 +4,11 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
+const productRoutes = require('../routes/products');
+const orderRoutes = require('../routes/orders');
 
-mongoose.connect('mongodb+srv://cbeck3:' + process.env.MONGO_ATLAS_PW +
-    '@mycluster-akj1d.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://andy_sa_account:' + process.env.MONGO_ATLAS_PW +
+    '@cluster0-kdf9e.azure.mongodb.net/test?retryWrites=true&w=majority',
     {
         useUnifiedTopology: true,
         useNewUrlParser: true
@@ -53,59 +53,3 @@ app.use((error, req, res, next) => {
 });
 
 module.exports = app;
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
-
-mongoose.connect('mongodb+srv://cbeck3:' + process.env.MONGO_ATLAS_PW +
-    '@mycluster-akj1d.mongodb.net/test?retryWrites=true&w=majority',
-    {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    }
-);
-    
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers',
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods',
-            'PUT, POST, PATCH, DELETE, GET'
-        );
-        return res.status(200).json({});
-    };
-    next();
-});
-
-// Routes which should handle requests
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
-
-app.use((req, res, next) => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
-});
-
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
-});
-
-module.exports = app;
-
