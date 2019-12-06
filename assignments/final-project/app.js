66d7743cffc4a5022ca71f9fd882b2d53dc80da9
Andy Kukuc <jeep98.ak@gmail.com>
2019-12-06 04:25:30 -0600

    everything is up and running fixing all errors and mistakes need to get a couple hours of sleep

---
+++ assignments/final-project/app.js
@@ -0,0 +1,50 @@
+var mongoose=require('mongoose');
+mongoose.Promise=global.Promise;
+mongoose.connect('mongodb+srv://andy_sa_account:5Y3FADcat5AqyvOM@cluster0-kdf9e.azure.mongodb.net/test?retryWrites=true&w=majority').then(()=>console.log("Connection Succesful!")).catch((err)=>console.log(err));
+
+
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+var students=require('./routes/students');
+app.use('/students',students);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//mongoose stuff
+module.exports = app;
