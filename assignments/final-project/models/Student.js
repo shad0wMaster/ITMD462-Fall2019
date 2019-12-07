26c112ab35b3445beb5a4532afded250cd10ece4
Andy Kukuc <jeep98.ak@gmail.com>
2019-12-07 12:32:15 -0600

    finsiehd up last night last thing left is css

--- assignments/final-project/models/Student.js
+++
@@ -1,8 +0,0 @@
-var mongoose=require('mongoose');
-var StudentSchema=new mongoose.Schema({
-  name:String,
-  ID_Number:Number,
-  Current_Year_In_School:Number,
-  updated_at:{type:Date,default:Date.now},
-});
-module.exports=mongoose.model('Student',StudentSchema);
